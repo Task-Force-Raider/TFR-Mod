b43c61f26e74e5b449af42aeac1d4c261fdd1b5b
BroBeans <BroBeansCPG@users.noreply.github.com>
2020-04-30 14:04:32 +0800

    Radio Backpack fix

---
+++ TFR_Radiopack_fix/config.cpp
@@ -0,0 +1,72 @@
+class cfgPatches
+{
+	class TFR_Radiobag_fix
+	{
+		units[]={"B_RadioBag_01_black_F","B_RadioBag_01_digi_F","B_RadioBag_01_eaf_F","B_RadioBag_01_ghex_F","B_RadioBag_01_hex_F","B_RadioBag_01_mtp_F","B_RadioBag_01_tropic_F","B_RadioBag_01_oucamo_F","B_RadioBag_01_wdl_F"};
+		weapons[]={};
+		requiredVersion=1.96;
+		requiredAddons[]={};
+		name="Task Force Raider - Radio Bags";
+		author="BroBeans";
+		url="https://discord.gg/JmTZmes";
+	};
+};
+
+class cfgVehicles
+{
+	class B_RadioBag_01_base_F;
+
+	class B_RadioBag_01_black_F: B_RadioBag_01_base_F
+	{
+	    maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_digi_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_eaf_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_ghex_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_hex_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_mtp_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_tropic_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_oucamo_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+
+	class B_RadioBag_01_wdl_F: B_RadioBag_01_base_F
+	{
+			maximumLoad = 140;
+			mass = 80;
+	};
+};
